12a818ef1e9d481b851b641d8599514d607194a6
achiever202 <es12b1001@iith.ac.in>
2015-05-23 20:49:22 +0530

    AC

--- DP/Longest Increasing Subsequence (nlogn).cpp
+++ DP/Longest Increasing Subsequence (nlogn).cpp
@@ -35,94 +35,96 @@
 
 using namespace std;
 
-/* The input array. */
-ll ar[max_size];
-
-/* 
- * lis_length[i] stores the index of the element with the smalles value for a subsequence of length i.
- * predecessor[i] stores the predecessor of the element at index i in the lis ending at i.
- * lis stores the longest increasing subsequence in reverse order.
- */
-vl lis_length;
-ll predecessor[max_size];
-vl lis;
-
-
-/* This function finds the largest index such that ar[lis_length[index]]<ar[cur]. */
-ll find_index(ll ar[], vector<ll> lis_length, ll cur, ll start, ll end)
+class LongestIncreasingSubsequence
 {
-	if(end-start<=1)
-	{
-		if(ar[lis_length[end]]<ar[cur])
-			return end;
+	public:
+		/* The input array. */
+		ll ar[max_size];
+		ll n;
+		/* 
+		 * lis_length[i] stores the index of the element with the smalles value for a subsequence of length i.
+		 * predecessor[i] stores the predecessor of the element at index i in the lis ending at i.
+		 * lis stores the longest increasing subsequence in reverse order.
+		 */
+		vl lis_length;
+		ll predecessor[max_size];
+		vl lis;
 
-		if(ar[lis_length[start]]<ar[cur])
-			return start;
-
-		return -1;
-	}
-
-	ll mid = (start+end)/2;
-
-	if(ar[lis_length[mid+1]]<ar[cur])
-		return find_index(ar, lis_length, cur, mid+1, end);
-
-	return find_index(ar, lis_length, cur, start, mid);
-}
-
-
-/* 
- * This function computes the longest increasing subsequence.
- * @param ar[]: the input array.
- * @param n: the size of array.
- * @return: the value of longest increasing subsequence.
- */
-ll  longest_increasing_subsequence(ll ar[], ll n)
-{
-	lis_length.push_back(0);
-	predecessor[0] = -1;
-
-	for(ll i=1; i<n; i++)
-	{
-		ll index = find_index(ar, lis_length, i, 0, lis_length.size()-1);
-
-		if(index==-1)
+		void init()
 		{
-			lis_length[0] = i;
-			predecessor[i] = -1;
+			cin>>n;
+			for(ll i=0; i<n; i++)
+				cin>>ar[i];
 		}
-		else if(index==lis_length.size()-1)
-		{
-			lis_length.push_back(i);
-			predecessor[i] = lis_length[index];
-		}
-		else
-		{
-			lis_length[index+1] = i;
-			predecessor[i] = lis_length[index];
-		}
-	}
 
-	ll cur = lis_length[lis_length.size()-1];
-	while(cur!=-1)
-	{
-		lis.push_back(ar[cur]);
-		cur = predecessor[cur];
-	}
+		/* This function finds the largest index such that ar[lis_length[index]]<ar[cur]. */
+		ll find_index(ll cur, ll start, ll end)
+		{
+			if(end-start<=1)
+			{
+				if(ar[lis_length[end]]<ar[cur])
+					return end;
 
-	return lis_length.size();
-}
+				if(ar[lis_length[start]]<ar[cur])
+					return start;
+
+				return -1;
+			}
+
+			ll mid = (start+end)/2;
+
+			if(ar[lis_length[mid+1]]<ar[cur])
+				return find_index(cur, mid+1, end);
+
+			return find_index(cur, start, mid);
+		}
+
+		/* 
+		 * This function computes the longest increasing subsequence.
+		 * @param ar[]: the input array.
+		 * @param n: the size of array.
+		 * @return: the value of longest increasing subsequence.
+		 */
+		ll  get_lis()
+		{
+			lis_length.push_back(0);
+			predecessor[0] = -1;
+
+			for(ll i=1; i<n; i++)
+			{
+				ll index = find_index(i, 0, lis_length.size()-1);
+
+				if(index==-1)
+				{
+					lis_length[0] = i;
+					predecessor[i] = -1;
+				}
+				else if(index==lis_length.size()-1)
+				{
+					lis_length.push_back(i);
+					predecessor[i] = lis_length[index];
+				}
+				else
+				{
+					lis_length[index+1] = i;
+					predecessor[i] = lis_length[index];
+				}
+			}
+
+			ll cur = lis_length[lis_length.size()-1];
+			while(cur!=-1)
+			{
+				lis.push_back(ar[cur]);
+				cur = predecessor[cur];
+			}
+
+			return lis_length.size();
+		}
+}longest_increasing_subsequence;
 
 int main()
 {
-	ll n;
-	cin>>n;
-
-	for(ll i=0; i<n; i++)
-		cin>>ar[i];
-
-	cout<<longest_increasing_subsequence(ar, n)<<endl;
-	for(ll i=lis.size()-1; i>=0; i--)
-		cout<<lis[i]<<" ";
-	cout<<endl;
+	longest_increasing_subsequence.init();
+	cout<<longest_increasing_subsequence.get_lis();
+	return 0;
 }
