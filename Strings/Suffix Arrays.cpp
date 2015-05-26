0ae13450960b218af54c1355a9e0036450f5a8c8
achiever202 <es12b1001@iith.ac.in>
2015-05-26 17:34:23 +0530

    Tested.

---
+++ Strings/Suffix Arrays.cpp
@@ -0,0 +1,120 @@
+/*
+ * Auhtor: Adarsh Pugalia
+ * Algorithm: Suffix Array construction in O(n*log^2(n))
+ *			  LCP calculation for suffixes in O(log(n))
+ * Implementation Credits: http://web.stanford.edu/class/cs97si/suffix-array.pdf
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <cmath>
+#include <cassert>
+ 
+#define ll long long int
+#define vi vector <int>
+#define vl vector <ll>
+#define vpii vector <pair<int, int> >
+#define vpll vector <pair<ll, ll> >
+#define pll pair<ll, ll>
+ 
+#define f first
+#define s second
+ 
+#define sd(n) scanf("%d", &n)
+#define sl(n) scanf("%lld", &n)
+ 
+#define pb push_back
+#define pob pop_back
+ 
+#define max_size 1005
+#define max_capacity INT_MAX
+#define max_string_size 1000
+#define max_node_size 340
+#define max_power 26
+#define max_sieve_size 5000003
+#define max_log 17
+ 
+#define MOD 100000000
+ 
+using namespace std;
+
+bool comp(pair<ll, pair<ll, ll> > i, pair<ll, pair<ll, ll> > j)
+{
+	if(i.s.f==j.s.f)
+		return i.s.s<j.s.s;
+
+	return i.s.f<j.s.f;
+}
+
+class SuffixArrays
+{
+	public:
+		string word;
+		ll step, cnt;
+		ll suf_ar[max_log][max_size];
+		pair<ll, pair<ll, ll> > sorted_sufs[max_size];
+
+		void init(string w)
+		{
+			word = w;
+			for(ll i=0; i<w.length(); i++)
+				suf_ar[0][i] = w[i]-'a';
+
+			for(step=1, cnt=1; cnt >> 1<w.length(); step++, cnt <<= 1)
+			{
+				for(ll i=0; i<w.length(); i++)
+				{
+					sorted_sufs[i].s.f = suf_ar[step-1][i];
+					sorted_sufs[i].s.s = (i+cnt<w.length())?suf_ar[step-1][i+cnt]:-1;
+					sorted_sufs[i].f = i;
+				}
+
+				sort(sorted_sufs, sorted_sufs+w.length(), comp);
+
+				for(ll i=0; i<w.length(); i++)
+				{
+					if(i>0 && sorted_sufs[i].s.f==sorted_sufs[i-1].s.f && sorted_sufs[i].s.s==sorted_sufs[i-1].s.s)
+						suf_ar[step][sorted_sufs[i].f] = suf_ar[step][sorted_sufs[i-1].f];
+					else
+						suf_ar[step][sorted_sufs[i].f] = i;
+				}
+			}
+		}
+
+		void print_suffixes()
+		{
+			for(ll i=0; i<word.length(); i++)
+			{
+				for(ll j=sorted_sufs[i].f; j<word.length(); j++)
+					cout<<word[j];
+				cout<<endl;
+			}
+		}
+
+		ll lcp(ll x, ll y)
+		{
+			if(x==y)
+				return word.length()-x;
+
+			ll ret = 0;
+
+			for(ll k=step-1; k>=0 && x<word.length() && y<word.length(); k--)
+				if(suf_ar[k][x]==suf_ar[k][y])
+					x += 1<<k, y += 1<<k, ret += 1<<k;
+
+			return ret;
+		}
+}suffix_array;
+
+int main()
+{
+	suffix_array.init("banana");
+	suffix_array.print_suffixes();
+	return 0;
+}
