0670f40ae3a3bd1a17819fdb8ef41a973e56005c
achiever202 <es12b1001@iith.ac.in>
2015-08-16 18:39:07 +0530

    AC

---
+++ Data Structures/RMQ (O(logn)).cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <cmath>
+#include <cassert>
+ 
+#define ll long long int
+#define vi vector <int>
+#define vl vector <ll>
+#define vpii vector <pair<int, int> >
+#define vpll vector <pair<ll, ll> >
+#define pll pair<ll, ll>
+ 
+#define f first
+#define s second
+ 
+#define sd(n) scanf("%d", &n)
+#define sl(n) scanf("%lld", &n)
+ 
+#define pb push_back
+#define pob pop_back
+ 
+#define max_size 10000005
+#define max_capacity INT_MAX
+#define max_string_size 1000
+#define max_node_size 340
+#define max_power 26
+ 
+#define MOD 1000000007
+ 
+using namespace std;
+
+class RMQ
+{
+	public:
+		ll num_of_elements;
+		ll power_of_two[max_power];
+		ll sparse_table[max_power][max_size];
+
+		RMQ()
+		{
+			power_of_two[0] = 1;
+			for(ll i=1; i<max_power; i++)
+				power_of_two[i] = power_of_two[i-1]*2;
+		}
+
+		void init(ll n, ll ar[])
+		{
+			num_of_elements = n;
+			for(ll i=0; i<num_of_elements; i++)
+				sparse_table[0][i] = ar[i];
+
+			for(ll j=1; j<max_power; j++)
+					for(ll i=0; i<n; i++)
+						sparse_table[j][i] = min(sparse_table[j-1][i], sparse_table[j-1][i+power_of_two[j-1]]);
+		}
+
+		ll compute_index(ll length)
+		{
+			ll index = 0;
+			while(power_of_two[index]<=length)
+				index++;
+
+			index--;
+
+			return index;
+		}
+
+		ll query(ll x, ll y)
+		{
+			if(x>y)
+			{
+				ll temp = x;
+				x = y;
+				y = temp;
+			}
+
+			ll length = y-x+1;
+			ll index = compute_index(y-x+1);
+			return min(sparse_table[index][x], sparse_table[index][y-power_of_two[index]+1]);			
+		}
+
+		ll query_index(ll x, ll y, ll index)
+		{
+			if(x>y)
+			{
+				ll temp = x;
+				x = y;
+				y = temp;
+			}
+
+			ll length = y-x+1;
+			return min(sparse_table[index][x], sparse_table[index][y-power_of_two[index]+1]);	
+		}
+}rmq;
+
+int main()
+{	
+	return 0;
+}
