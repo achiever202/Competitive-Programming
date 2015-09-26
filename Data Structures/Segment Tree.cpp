7ada1219d258f6f104019754a327e6879a379d3b
achiever202 <es12b1001@iith.ac.in>
2015-09-26 22:04:33 +0530

    Changed ll to int.

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -8,49 +8,68 @@
 #include <stack>
 #include <cmath>
 #include <cassert>
+#include <map>
+#include <limits>
+#include <cassert>
  
 #define ll long long int
 #define vi vector <int>
-#define vl vector <ll>
-#define vpii vector <pair<int, int> >
-#define vpll vector <pair<ll, ll> >
+#define vl vector <ll> 
+#define pii pair<int, int>
 #define pll pair<ll, ll>
+#define vpii vector <pii >
+#define vpll vector <pll >
+ 
  
 #define f first
 #define s second
  
 #define sd(n) scanf("%d", &n)
 #define sl(n) scanf("%lld", &n)
- 
+#define slf(n) scanf("%lf", &n)
+
+#define pd(n) printf("%d", n);
+#define pl(n) printf("%lld", n);
+#define ps printf(" ")
+#define pe printf("\n")
+
+#define rep(i,j,k) for(int i=j; i<=k; i++)
+#define repd(i,j,k) for(int i=j; i>=k; i--)
+
+#define sz(n) (int)n.size()-1 
+#define all(n) n.begin(), n.end()
 #define pb push_back
 #define pob pop_back
+#define mp make_pair
  
 #define max_size 100005
 #define max_capacity INT_MAX
 #define max_string_size 1000
 #define max_node_size 340
 #define max_power 26
-#define max_sieve_size 5000003
- 
-#define MOD 100000000
+#define max_log 17
+#define max_sieve_size 1005
+
+#define INF 100000000000L
+#define MOD 1000000007
  
 using namespace std;
 
 class SegmentTree
 {
 	public:
-		ll n;
+		int n;
 		ll ar[max_size];
 		ll seg_tree[4*max_size];
 
-		void init()
+		void init(int num)
 		{
-			scanf("%d %d", &n, &q);
-			for(ll i=1; i<=n; i++)
-				scanf("%d", &ar[i]);
+			n = num;
+			rep(i,1,n)
+				sd(ar[i]);
 		}
 
-		ll build_segment_tree(ll node, ll start, ll end)
+		ll build_segment_tree(int node, int start, int end)
 		{
 			if(start==end)
 			{
@@ -58,7 +77,7 @@ class SegmentTree
 				return seg_tree[node];
 			}
 
-			ll mid = (start+end)/2;
+			int mid = (start+end)/2;
 			ll left = build_segment_tree(2*node, start, mid);
 			ll right = build_segment_tree(2*node+1, mid+1, end);
 
@@ -67,14 +86,14 @@ class SegmentTree
 			return seg_tree[node];
 		}
 
-		ll query(ll node, ll start, ll end, ll x, ll y)
+		ll query(int node, int start, int end, int x, int y)
 		{
-			if(end==start && end==x && end==y)
+			if(start==x && end==y)
 			{
 				return seg_tree[node];
 			}
 
-			ll mid = (start+end)/2;
+			int mid = (start+end)/2;
 
 			if(mid>=y)
 				return query(2*node, start, mid, x, y);
@@ -90,7 +109,7 @@ class SegmentTree
 			return seg_tree[node];
 		}
 
-		void print_segment_tree(ll node, ll start, ll end)
+		void print_segment_tree(int node, int start, int end)
 		{
 			if(start==end)
 			{
@@ -98,7 +117,7 @@ class SegmentTree
 				return;
 			}
 
-			ll mid = (start+end)/2;
+			int mid = (start+end)/2;
 			print_segment_tree(2*node, start, mid);
 			print_segment_tree(2*node+1, mid+1, end);
 
