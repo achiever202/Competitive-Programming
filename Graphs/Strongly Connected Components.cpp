ae21336af22d1fb478c7b69fb2b7a6111bda6e86
achiever202 <es12b1001@iith.ac.in>
2015-11-15 15:38:02 +0530

    Template changed.

--- Graphs/Strongly Connected Components.cpp
+++ Graphs/Strongly Connected Components.cpp
@@ -16,8 +16,11 @@
 #include <cassert>
 #include <map>
 #include <limits>
+#include <cassert>
+#include <string.h>
  
-#define ll int
+#define ll long long int
+#define llu long long int unsigned
 #define vi vector <int>
 #define vl vector <ll> 
 #define pii pair<int, int>
@@ -32,142 +35,90 @@
 #define sd(n) scanf("%d", &n)
 #define sl(n) scanf("%lld", &n)
 #define slf(n) scanf("%lf", &n)
-
+ 
 #define pd(n) printf("%d", n);
+#define pl(n) printf("%lld", n);
 #define ps printf(" ")
 #define pe printf("\n")
-
+ 
 #define rep(i,j,k) for(int i=j; i<=k; i++)
 #define repd(i,j,k) for(int i=j; i>=k; i--)
-
-#define sz(n) (int)n.size()-1 
+ 
+#define sz(n) (int)n.size()-1
+#define all(n) n.begin(), n.end()
+ 
 #define pb push_back
 #define pob pop_back
 #define mp make_pair
  
 #define max_size 100005
-#define max_capacity INT_MAX
-#define max_string_size 1000
-#define max_node_size 50005
-#define max_power 26
-#define max_sieve_size 105
+#define max_sieve_size 100005
 #define max_log 17
-
-#define INF 10000000
+ 
+#define INF 1000000000
 #define MOD 1000000007
  
+#define mod(a) ((a)%MOD)
+ 
 using namespace std;
-
-class Edge
-{
-	public:
-		int node;
-		ll cost;
-		int index;
-
-		Edge(int i, ll j)
-		{
-			node = i;
-			cost = j;
-		}
-
-		Edge(int i, ll j, int k)
-		{
-			node = i;
-			cost = j;
-			index = k;
-		}
-};
-
-bool edge_comp(Edge i, Edge j)
-{
-	return i.node<j.node;
-}
+ 
+using namespace std;
 
 class Node
 {
 	public:
 		int index;
 		int parent;
-		ll cost;
-		int colour;
-		vector <Edge> edges;
+		vector <pair<int, ll> > edges;
 
 		Node()
 		{
 
 		}
-
-		Node(int i)
-		{
-			index = i;
-			parent = -1;
-		}
-
-		void add_edge(int node, ll cost)
-		{
-			edges.pb(Edge(node, cost));
-		}
-
-		void add_edge(int node, ll cost, int index)
-		{
-			edges.pb(Edge(node, cost, index));
-		}
 };
 
-bool node_comp(Node i, Node j)
-{
-	return i.index<j.index;
-}
-
-class Graph
-{
+class Graph {
 	public:
-		int node_size;
-		int edge_size;
-		Node nodes[max_node_size];
-		int visited[max_node_size];
+		Node nodes[max_size];
+		int node_size, edge_size;
+		int visited[max_size];
 
-		Graph()
-		{
+		void init(int node_sz, int edge_sz) {
+			node_size = node_sz, edge_size = edge_size;
 
-		}
-
-		void init(ll n, ll m)
-		{
-			node_size = n;
-			edge_size = m;
-			for(ll i=0; i<=n; i++)
-			{
+			rep(i,1,node_size) {
 				nodes[i].index = i;
 				nodes[i].parent = -1;
 				nodes[i].edges.clear();
-				nodes[i].colour = -1;
+				visited[i] = 0;
 			}
 		}
 
-		void add_edge(ll i, ll j, ll k)
-		{
-			nodes[i].add_edge(j, k);
+		void add_edge(int i, int j) {
+			nodes[i].edges.pb(mp(j,1));
 		}
 
-		void add_edge(ll i, ll j, ll k, ll m)
-		{
-			nodes[i].add_edge(j, k, m);
+		void add_edge(int i, int j, ll cost) {
+			nodes[i].edges.pb(mp(j,cost));
 		}
 
-		void add_bidirectional_edge(ll i, ll j, ll k)
-		{
-			nodes[i].add_edge(j, k);
-			nodes[j].add_edge(i, k);
+		void add_bidirectional_edge(int i, int j) {
+			add_edge(i,j);
+			add_edge(j,i);
 		}
 
-		void clear_graph()
-		{
-			for(ll i=0; i<max_node_size; i++)
-			{
-				nodes[i].parent = -1;
-				nodes[i].edges.clear();
+		void add_bidirectional_edge(int i, int j, ll cost) {
+			add_edge(i, j, cost);
+			add_edge(j,i,cost);
+		}
+
+		void dfs(int cur_node, int parent) {
+			visited[cur_node] = 1;
+
+			rep(i,0,sz(nodes[cur_node].edges)) {
+				int next_node = nodes[cur_node].edges[i].f;
+				if(!visited[next_node])
+					dfs(next_node, cur_node);
 			}
 		}
 }graph;
@@ -178,16 +129,20 @@ class StronglyConnectedComponents {
 		Graph graph, reversed_graph;
 		stack <int> nodes;
 		int visited[max_size];
+		vector<vector<int> > scc;
+		int cur_scc_count;
 
-		void init(int n, int m) {
-			graph.init(n,m);
+		void init(Graph g) {
+			graph = g;
+			reverse_graph();
+			cur_scc_count = 0;
 		}
 
 		void reverse_graph() {
 			reversed_graph.init(graph.node_size, graph.edge_size);
 			rep(i,1,graph.node_size) {
 				rep(j,0,sz(graph.nodes[i].edges)) {
-					reversed_graph.add_edge(graph.nodes[i].edges[j].node, i, 1);
+					reversed_graph.add_edge(graph.nodes[i].edges[j].f, i, graph.nodes[i].edges[j].s);
 				}
 			}
 		}
@@ -198,22 +153,22 @@ class StronglyConnectedComponents {
 
 			if(flag) {
 				rep(i,0,sz(graph.nodes[node].edges)) {
-					if(!visited[graph.nodes[node].edges[i].node])
-						recursive_dfs(graph.nodes[node].edges[i].node, node, 1);
+					if(!visited[graph.nodes[node].edges[i].f])
+						recursive_dfs(graph.nodes[node].edges[i].f, node, 1);
 				}
 
 				nodes.push(node);
 			}
 			else {
+				scc[cur_scc_count].pb(node);
 				rep(i,0,sz(reversed_graph.nodes[node].edges)) {
-					if(!visited[reversed_graph.nodes[node].edges[i].node])
-						recursive_dfs(reversed_graph.nodes[node].edges[i].node, node, 0);
+					if(!visited[reversed_graph.nodes[node].edges[i].f])
+						recursive_dfs(reversed_graph.nodes[node].edges[i].f, node, 0);
 				}
 			}
 		}
 
-		int get_scc_count() {
-			int ret = 0;
+		void compute_scc() {
 			fill(visited, visited+graph.node_size+1, 0);
 
 			rep(i,1,graph.node_size) {
@@ -227,16 +182,16 @@ class StronglyConnectedComponents {
 				nodes.pop();
 
 				if(!visited[cur]) {
-					ret++;
+					vector<int> v;
+					scc.pb(v);
+
 					recursive_dfs(cur, -1, 0);
+					cur_scc_count++;
 				}
 			}
-
-			return ret;
 		}
 }scc;
 
 int main() {
-
 	return 0;
 }
